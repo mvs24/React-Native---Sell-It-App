c156c62743507dddb8b1a524ba4f60d218eddaf0
Marius <Marius.Vasili@fti.edu.al>
2019-11-04 18:49:31 +0100

    Ordered Screen

--- screens/OrderedScreen.js
+++ screens/OrderedScreen.js
@@ -1,2 +1,8 @@
 import React from 'react';
-import {View} from 'react-native'
+import {View} from 'react-native';
+
+const OrderedScreen = props => {
+    return ( null )
+}
+
+export default OrderedScreen;
